dd8f66c60eba6876ab26cceb52f436110444d175
GiacomoGaudino <giacomo.gaudino@yahoo.it>
2025-07-05 09:40:47 +0200

    variables bonus saturday 02 done

---
+++ 05-bonus-saturday-2/scritpt.js
@@ -0,0 +1,31 @@
+// 1. Dichiarazione di variabili
+// Crea una variabile chiamata nickname e assegnale un soprannome.
+// Stampa il valore di nickname nella console.
+const nickname = "pipe de oro"
+console.log(nickname)
+
+// 2. Tipi primitivi
+// Crea tre variabili chiamate luckyNumber, phrase e isCodingFun
+// e assegna rispettivamente un numero, una stringa e un valore booleano.
+// Stampa tutti i valori nella console.
+const luckyNumber = (11)
+console.log(luckyNumber)
+const phrase = "giorno speciale"
+console.log(phrase)
+const isCodingFun = 8 < 9
+console.log(isCodingFun)
+
+// 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
+// e le variabili sopra in modo da stampare correttamente 
+// in console la stringa con il soprannome completo
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // NON MODIFICARE QUESTA RIGA
+const userName = 'Alex'; // NON MODIFICARE QUESTA RIGA
+const userNickname = 'Thunder'; // NON MODIFICARE QUESTA RIGA
+
+const completeName = userName + " " + userNickname;
+console.log(completeName); // Output: "Alex Thunder"
+
+// 4. Imposta questa variabile utilizzando una proprietà
+// sulla variabile characters per ottenere la sua lunghezza
+const alphabetSize = "l'inter era ingiocabile xD";
+console.log(alphabetSize.length); // Output: 26
